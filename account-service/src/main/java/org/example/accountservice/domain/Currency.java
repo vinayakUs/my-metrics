110548e253cf1cd0e42acc8bdb06f1b06e93c2e7
vinayakUs <vinayak200054321@gmail.com>
2025-06-16 10:42:38 +0530

    account service dto

---
+++ account-service/src/main/java/org/example/accountservice/domain/Currency.java
@@ -0,0 +1,10 @@
+package org.example.accountservice.domain;
+
+public enum Currency {
+
+	USD, EUR, RUB;
+
+	public static Currency getDefault() {
+		return USD;
+	}
+}
---
+++ account-service/src/main/java/org/example/accountservice/domain/TimePeriod.java
@@ -0,0 +1,7 @@
+package org.example.accountservice.domain;
+
+public enum TimePeriod {
+
+	YEAR, QUARTER, MONTH, DAY, HOUR
+
+}
